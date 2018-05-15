a09a3c737bdf7d23e2d61fd4f842d41f1f94917a
JAIR AXEL Lizarraga <jair.lizarraga@ibm.com>
2018-05-15 09:16:10 -0500

    Bootstrap prueba

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-sudo apt-get update
+
+apt-get update
+apt-get install espeak
+
+read -p "Would you like to change your RaspberryPI password? [Y/n] " choice </dev/tty
+case "$choice" in
+    "y" | "Y")
+        passwd
+        ;;
+    *) ;;
+esac
+passwd
