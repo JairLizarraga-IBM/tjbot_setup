8cc34003ed3e867be8a5ab5cdd9c9440df8de137
JAIR AXEL Lizarraga <jair.lizarraga@ibm.com>
2018-05-21 18:09:22 -0500

    Script comprobado paso por paso

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,21 @@
 
 #Update dependences and install text to speech app
 apt-get update
-apt-get install espeak
+yes | apt-get install espeak
 
 #Enable speakable ip address
 mkdir -p /home/pi/scripts
 cp getipaddress.sh /home/pi/scripts/
-sed -i -e "s/exit 0;/sleep 15; \/home\/pi\/scripts\/getipaddress.sh || exit 1; exit 0;/g" /etc/rc.local
+sed -i -e "s/exit 0/sleep 15; \/home\/pi\/scripts\/getipaddress.sh || exit 1; exit 0;/g" /etc/rc.local
 
 #Enable Wi-Fi bootable configuration
 cp mi_red_wifi.default.txt /boot/
 cp mi_red_wifi.txt /boot/
 
 #Enable ssh to start at boot
-sudo systemctl enable ssh
-sudo systemctl start ssh
-
+systemctl enable ssh
+systemctl start ssh
+#!!!
 read -p "Would you like to change your RaspberryPI password? [Y/n] " choice </dev/tty
 case "$choice" in
     "y" | "Y")
@@ -26,7 +26,7 @@ case "$choice" in
 esac
 
 #Habilitar camara
-sed -i -e "s/start_x=0/start_x=1/g" /boot/config.txt
+echo "start_x=1" | tee -a /boot/config.txt
 #Set audio output to automatic
 amixer cset numid=3 0
 #Set volume to 50%
@@ -34,7 +34,7 @@ amixer sset 'PCM' 50%
 
 # Node red update/install
 node-red-stop
-cd /home/pi/.node-red #!!!
+cd /home/pi/.node-red
 mkdir /home/pi/.node-red/nodes
 cd /home/pi/.node-red/nodes
 git clone https://github.com/jeancarl/node-red-contrib-tjbot
@@ -42,14 +42,11 @@ cd /home/pi/.node-red/nodes/node-red-contrib-tjbot
 npm install --unsafe-perm
 
 # Node red service configuration
-sudo sed -i -e 's/User=pi/User=root/g' /lib/systemd/system/nodered.service
-sudo systemctl daemon-reload
-sudo systemctl enable nodered.service
-# Cerrar ventana y abrir otra
-node-red-start
+sed -i -e 's/User=pi/User=root/g' /lib/systemd/system/nodered.service
+systemctl daemon-reload
+systemctl enable nodered.service
 
-node-red-stop
 # Renombrar usuario en javascript
-sed -i -e "s/\/\/userDir: ‘\/home\/nol\/.node-red\/nodes’ ,/userDir: ‘\/home\/pi\/.node-red\/’ ,/g" /root/.node-red/settings.js
-sed -i -e "s/\/\/nodesDir: ‘\/home\/nol\/.node-red\/nodes’ ,/nodesDir: ‘\/home\/pi\/.node-red\/nodes’ ,/g" /root/.node-red/settings.js
+sed -i -e "s/\/\/userDir: '\/home\/nol\/.node-red\/'/userDir: '\/home\/pi\/.node-red\/'/g" /root/.node-red/settings.js
+sed -i -e "s/\/\/nodesDir: '\/home\/nol\/.node-red\/nodes',/nodesDir: '\/home\/pi\/.node-red\/nodes',/g" /root/.node-red/settings.js
 node-red-start
