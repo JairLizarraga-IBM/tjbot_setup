c9aa6802be1666d7899cc3e7eb1ec14e2ac40c9d
JAIR AXEL Lizarraga <jair.lizarraga@ibm.com>
2018-05-21 16:38:33 -0500

    script para nodered

--- node.sh
+++ node.sh
@@ -1,1 +1,1 @@
-curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered
+bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
