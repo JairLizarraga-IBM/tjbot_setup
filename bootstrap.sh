97caa77edd6392e56c18fa924d2f1583b7f36fff
JAIR AXEL Lizarraga <jair.lizarraga@ibm.com>
2018-05-21 15:00:11 -0500

    Files update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+#Update dependences and install text to speech app
 apt-get update
 apt-get install espeak
+
+#Enable speakable ip address
+mkdir -p /home/pi/scripts
+cp getipaddress.sh /home/pi/scripts/
+sed -i -e "s/exit 0;/sleep 15; \/home\/pi\/scripts\/getipaddress.sh || exit 1; exit 0;/g" /etc/rc.local
+
+#Enable Wi-Fi bootable configuration
+cp mi_red_wifi.default.txt /boot/
+cp mi_red_wifi.txt /boot/
+
+#Enable ssh to start at boot
+sudo systemctl enable ssh
+sudo systemctl start ssh
 
 read -p "Would you like to change your RaspberryPI password? [Y/n] " choice </dev/tty
 case "$choice" in
@@ -18,24 +32,24 @@ amixer cset numid=3 0
 #Set volume to 50%
 amixer sset 'PCM' 50%
 
-
 # Node red update/install
 node-red-stop
-cd .node-red
-mkdir nodes
-cd nodes
+cd /home/pi/.node-red #!!!
+mkdir /home/pi/.node-red/nodes
+cd /home/pi/.node-red/nodes
 git clone https://github.com/jeancarl/node-red-contrib-tjbot
-cd node-red-contrib-tjbot
+cd /home/pi/.node-red/nodes/node-red-contrib-tjbot
 npm install --unsafe-perm
 
 # Node red service configuration
-sudo sed -i -e 's/User=pi/User=root/g' ex.service
+sudo sed -i -e 's/User=pi/User=root/g' /lib/systemd/system/nodered.service
 sudo systemctl daemon-reload
+sudo systemctl enable nodered.service
 # Cerrar ventana y abrir otra
 node-red-start
 
 node-red-stop
 # Renombrar usuario en javascript
-sed -i -e "s/\/\/userDir: ‘\/home\/nol\/.node-red\/nodes’ ,/userDir: ‘\/home\/pi\/.node-red\/’ ,/g" ex.js
-sed -i -e "s/\/\/nodesDir: ‘\/home\/nol\/.node-red\/nodes’ ,/nodesDir: ‘\/home\/pi\/.node-red\/nodes’ ,/g" ex.js
+sed -i -e "s/\/\/userDir: ‘\/home\/nol\/.node-red\/nodes’ ,/userDir: ‘\/home\/pi\/.node-red\/’ ,/g" /root/.node-red/settings.js
+sed -i -e "s/\/\/nodesDir: ‘\/home\/nol\/.node-red\/nodes’ ,/nodesDir: ‘\/home\/pi\/.node-red\/nodes’ ,/g" /root/.node-red/settings.js
 node-red-start
