eb526a5a88a5e368bd2478438e06e474bba55ffc
Jair Lizarraga <lga.jair@gmail.com>
2018-08-07 16:20:42 +0000

    Cambios en el archivo getipaddres para una voz mas natural. Simplificacion del algoritmo. El algoritmo obtiene la direccion IP de una red alambrica e inalambrica

--- getipaddress.sh
+++ getipaddress.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
-ROUTE="/home/pi/ipaddress.txt"
 BOOT_WPA="/boot/mi_red_wifi.txt"
 WPA_SUPPLICANT="/etc/wpa_supplicant/wpa_supplicant.conf"
-IP="$(ifconfig wlan0 | grep 'inet ' | cut -d: -f2 | awk '{print $2}')"
-MESSAGE="Tu direcciòn I PE es"
+IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | sed -r 's/\./ punto /g')
+
+### Voice parameters ###
 VOLUME=30
-SPEED=175
-PINCH=35
-PAUSA=10
-ENFASIS=20
-LENGUAJE=es-la+f3
+SPEED=200
+PINCH=50
+PAUSA=15
+ENFASIS=30
+LENGUAJE=es+m5
 
 # Busca si existe una nueva red agregada manualmente por el usuario
 NEWSSID="$(grep 'ssid' /boot/mi_red_wifi.txt)"
-if [ ${#NEWSSID} -gt 15 ]
+if [ ${#NEWSSID} -gt 15 ];
 then
 	cat $BOOT_WPA >> $WPA_SUPPLICANT
 	cp /boot/mi_red_wifi.default.txt /boot/mi_red_wifi.txt
-        espeak "Nueva configuración wai fai detectada. Reiniciando." -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE
+        espeak "Nueva configuración wai fai detectada. Reiniciando." -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE -s $SPEED
 	sudo reboot
 fi
 
 # Se vacia la direccion IP en archivo, para modificar su texto y poder hablarlo
-> $ROUTE
-echo $IP > $ROUTE
-sed -i 's/\./ punto /g' $ROUTE
-if [ ${#IP} -gt 0 ]
+if [ ! -z "$IP" ];
 then
-        SSID="$(iwgetid -r)"
-        espeak "Te has conectado a $SSID" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE
-	espeak "$MESSAGE" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE
-        espeak -f $ROUTE  -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE
-	ping www.google.com -c 1
-	if [ "$?" -gt 0 ]
+	if [[ $(cat /sys/class/net/eth0/operstate) -eq 'up' ]];
 	then
-		espeak "Conexiòn a internet fallida" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE
+		 espeak "Iniciando con conexion alambrica" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE -s $SPEED
 	else
-		espeak "Conexiòn a internet establecida" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE
+        	espeak "Conexion wai fai a la red $(iwgetid -r)" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE -s $SPEED
+	fi
+	espeak "Tu direccion I PE es... $IP, tu direccion I PE es... $IP" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE -s $SPEED
+	ping www.google.com -c 1
+	if [ "$?" -gt 0 ];
+	then
+		espeak "Conexiòn a internet fallida" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE -s $SPEED
+	else
+		espeak "Conexiòn a internet establecida" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE -s $SPEED
 		nc -z -v -w5 127.0.0.1 1880
-		if [ "$?" -eq 0 ]
+		if [ "$?" -eq 0 ];
 		then
-        		espeak "Servidor iniciado" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE
+        		espeak "Servidor iniciado" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE -s $SPEED
 		fi
 	fi
 else
-	espeak "Error en la conexiòn a internet" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE
+	espeak "Error en la conexiòn a internet" -k $ENFASIS -a $VOLUME  -p $PINCH -g $PAUSA -v $LENGUAJE -s $SPEED
 fi
