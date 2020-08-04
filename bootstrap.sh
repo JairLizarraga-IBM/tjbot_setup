f5085a556ea5e83a0b2d2aafb5e71fbeca0fc6a1
Jair Lizarraga <alizarra@mx1.ibm.com>
2020-08-04 14:02:43 -0500

    actualizado script para Raspberry OS

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,12 @@
 # Script that enables TJBot project in a new recent Raspbian OS installation.
 
 #Update dependences and install text to speech app
-installResources(){
+installAccesibility(){
 	apt-get install espeak -y
 	apt-get install speech-dispatcher -y
 	apt-get install matchbox-keyboard -y
-	cp ./config_files/keyboard.sh /home/pi/Desktop/
+	cp /home/pi/tjbot_setup/config_files/keyboard.sh /home/pi/Desktop/
+	chmod +x /home/pi/Desktop/keyboard.sh
 }
 
 # Enable ssh to start at boot
@@ -15,29 +16,27 @@ enableSSH(){
 	systemctl start ssh
 }
 
-configureHardware(){
+# Activate camera and set audio default output
+configureHardwareComponents(){
 	echo "start_x=1" | tee -a /boot/config.txt # Activating camera
+	cp /home/pi/tjbot_setup/config_files/sound.blacklist.conf /etc/modprobe.d/sound.blacklist.conf # Set USB Audio as default
 	amixer cset numid=3 0 # #Set audio output to automatic
-	cp ./config_files/sound.blacklist.conf /etc/modprobe.d/sound.blacklist.conf # Set USB Audio as default
 	amixer sset 'PCM' 100% # Set volume to 100%
 }
 
 # Update nodejs and node-red
 updateNode(){
-	git clone https://github.com/node-red/raspbian-deb-package.git ./config_files/node-red
-	cd ./config_files/node-red/resources/
-	su pi -c " ./update-nodejs-and-nodered" # Executing with user pi
-	echo "Finalizando actualizacíon nodejs y nodered"
-	sleep 5
+	git clone https://github.com/node-red/linux-installers.git /home/pi/tjbot_setup/config_files/
+	su pi -c " /home/pi/tjbot_setup/config_files/linux-installers/deb/update-nodejs-and-nodered" # Executing with user pi
+	echo "Finalizada actualizacíon nodejs y nodered"
 }
+
 
 # Installing TJBot nodes
 installTJNodes(){
 	node-red-stop
-	mkdir ~/.node-red/nodes
-	git clone https://github.com/JairLizarraga/nodes-tjbot-latam ~/.node-red/nodes
-	cd ~/.node-red/nodes/nodes-tjbot-latam
-	npm install --unsafe-perm
+	git clone https://github.com/JairLizarraga/nodes-tjbot-latam.git /home/pi/.node-red/nodes/nodes-tjbot-latam
+	npm install --unsafe-perm /home/pi/.node-red/nodes/nodes-tjbot-latam
 }
 
 
@@ -60,12 +59,14 @@ configureNodeRedService(){
 
 #Enable Wi-Fi bootable configuration
 enableAddWiFiFromBoot(){
-	cp ~/tjbot_setup/config_files/mi_red_wifi.default.txt /boot/
-	cp ~/tjbot_setup/config_files/mi_red_wifi.txt /boot/
-	cp ~/tjbot_setup/config_files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.default.conf
+	cp /home/pi/tjbot_setup/config_files/mi_red_wifi.default.txt /boot/
+	cp /home/pi/tjbot_setup/config_files/mi_red_wifi.txt /boot/
+	cp /home/pi/tjbot_setup/config_files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.default.conf
 }
 
-installResources
+apt-get update
+apt-get upgrade
+installAccesibility
 enableSSH
 configureHardware
 updateNode
@@ -73,6 +74,6 @@ installTJNodes
 configureNodeRedService
 enableAddWiFiFromBoot
 # Enable to speak IP address using crontab using pi user
-su pi -c "(crontab -l 2>/dev/null; echo '@reboot ~/tjbot_setup/config_files/getipaddress.sh') | crontab -"
+su pi -c "(crontab -l 2>/dev/null; echo '@reboot /home/pi/tjbot_setup/config_files/getipaddress.sh') | crontab -"
 
 reboot now
